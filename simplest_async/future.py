37ae3e84848924810e2222d705d7842a58498b21
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-03-29 16:19:23 +0900

    Add future.py

---
+++ simplest_async/future.py
@@ -0,0 +1,63 @@
+from .loop import EventLoop
+from typing import List, Callable
+
+_PENDING = 0
+_FINISHED = 1
+_CANCELLED = -1
+
+
+class ResultNotExist(Exception):
+    pass
+
+
+class FutureCancelled(Exception):
+    pass
+
+
+class AwaitBeforeDone(Exception):
+    pass
+
+
+class Future:
+    _STATE = _PENDING
+    _callbacks: List[Callable]
+    _loop: EventLoop
+
+    def __init__(self, _loop: EventLoop):
+        self._loop = _loop
+        self._callbacks = []
+
+    def _run_callbacks(self):
+        raise NotImplementedError()
+
+    def add_callback(self, _callback: Callable):
+        self._callbacks.append(_callback)
+
+    def set_result(self, _result):
+        self._result = _result
+        self._STATE = _FINISHED
+        self._run_callbacks()
+
+    def cancel(self):
+        if self._STATE == _PENDING:
+            self._STATE = _CANCELLED
+            self._run_callbacks()
+
+    def result(self):
+        if self._STATE == _PENDING:
+            raise ResultNotExist()
+        elif self._STATE == _CANCELLED:
+            raise FutureCancelled()
+        return self._result
+
+    def is_done(self) -> bool:
+        if self._STATE == _PENDING:
+            return False
+        return True
+
+    def __await__(self):
+        if not self.is_done():
+            return self
+        if not self.is_done():
+            raise AwaitBeforeDone()
+        return self.result()
