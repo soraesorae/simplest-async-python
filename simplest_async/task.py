a31ee4bacf78efbb69f837f95574b2f97aa2c682
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-03-29 21:31:14 +0900

    Add _step(), _wakeup() to Task
    add _step(), _wakeup()

--- simplest_async/task.py
+++ simplest_async/task.py
@@ -1,9 +1,28 @@
 from .loop import EventLoop
 from .future import Future
+from typing import Coroutine
 
 
 class Task(Future):
     _loop: EventLoop
+    _coroutine: Coroutine
+    _current_wait_fut: Future | None
 
-    def __init__(self, _loop: EventLoop):
+    def __init__(self, _loop: EventLoop, _coroutine: Coroutine):
         super().__init__(_loop)
+        # TODO: check _coroutine is Coroutine
+        self._coroutine = _coroutine
+        self._current_wait_fut = None
+
+    def _step(self):
+        try:
+            res = self._coroutine.send(None)
+        except StopIteration as e:
+            pass
+        else:
+            if isinstance(res, Future):
+                self._current_wait_fut = res
+                res.add_callback(self._wakeup)
+
+    def _wakeup(self, _fut: Future):
+        raise NotImplementedError()
