7c17aa29807fb927feaa1bf7bdc683ae57429b6f
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-03-29 21:03:38 +0900

    Implement Handle, TimerHandle class

--- simplest_async/handle.py
+++ simplest_async/handle.py
@@ -1,3 +1,31 @@
+from typing import Callable
+
+
 class Handle:
-    def __init__(self):
-        pass
+    _callback: Callable
+
+    def __init__(self, _callback: Callable, *_args):
+        self._callback = _callback
+        self._args = _args
+
+    def run(self):
+        try:
+            self._callback(*self._args)
+        except:
+            raise
+
+
+class TimerHandle(Handle):
+    _start_time: int
+
+    def __init__(self, _start_time: int, _callback, *_args):
+        super().__init__(_callback, *_args)
+        self._start_time = _start_time
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    @start_time.setter
+    def start_time(self, _start_time: int):
+        self._start_time = _start_time
