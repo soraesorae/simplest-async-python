8a4ed0243a15c55d0363606abf0b8a9e5db51747
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-03-30 11:20:44 +0900

    Fix var name in EventLoop

--- simplest_async/loop.py
+++ simplest_async/loop.py
@@ -1,15 +1,17 @@
 from collections import deque
 import heapq
+import time
 from .handle import Handle, TimerHandle
 from typing import List, Callable
 
 
 class EventLoop:
     _callback_queue: deque[Handle | TimerHandle]
-    _timer_callback_queue: List[TimerHandle]
+    _timer_callback_heap: List[TimerHandle]
 
     def __init__(self):
         self._callbacks = deque()
+        self._timer_callback_heap = []
 
     def push_callback(self, _callback: Callable, *_args):
         handle = Handle(_callback, *_args)
@@ -17,4 +19,7 @@ class EventLoop:
 
     def push_timer_callback(self, _start_time: int, _callback: Callable, *_args):
         t_handle = TimerHandle(_start_time, _callback, *_args)
-        heapq.heappush(self._timer_callback_queue, t_handle)
+        heapq.heappush(self._timer_callback_heap, t_handle)
+
+    def _round(self):
+        current_ts = time.monotonic()
