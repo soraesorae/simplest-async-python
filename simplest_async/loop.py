bbe10e557e64f86ff79cdabf533ad995f42997b2
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-03-30 10:05:21 +0900

    Add push_callback and push_timer_callback in order to manage callback handle

--- simplest_async/loop.py
+++ simplest_async/loop.py
@@ -1,11 +1,20 @@
 from collections import deque
+import heapq
+from .handle import Handle, TimerHandle
+from typing import List, Callable
 
 
 class EventLoop:
-    _callbacks: deque
+    _callback_queue: deque[Handle | TimerHandle]
+    _timer_callback_queue: List[TimerHandle]
 
     def __init__(self):
         self._callbacks = deque()
 
-    def push_callback(self, _callback):
-        self._callbacks.append(_callback)
+    def push_callback(self, _callback: Callable, *_args):
+        handle = Handle(_callback, *_args)
+        self._callbacks.append(handle)
+
+    def push_timer_callback(self, _start_time: int, _callback: Callable, *_args):
+        t_handle = TimerHandle(_start_time, _callback, *_args)
+        heapq.heappush(self._timer_callback_queue, t_handle)
